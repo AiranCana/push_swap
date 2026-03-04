a32fc37ec2a9b10e7e0fba90c71a63d9dbe86ffa
raqroca- <raqroca-@student.42madrid.com>
2026-03-04 12:20:52 +0100

    comments in utils_sort_complex

--- utils_sort_complex.c
+++ utils_sort_complex.c
@@ -6,11 +6,31 @@
 /*   By: raqroca- <raqroca-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 11:35:12 by raqroca-          #+#    #+#             */
-/*   Updated: 2026/03/04 11:44:39 by raqroca-         ###   ########.fr       */
+/*   Updated: 2026/03/04 12:19:58 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+/*
+** Sort 3 elements by comparing their positions.
+** If they are already sorted (first < second < third), finish.
+** If the 1º is the max, rotate it backwards and check if it needs to be swapped.
+** If the 2º is the max, rotate it backwards and check if it needs to be swapped.
+** In any other case, just swap the first two.
+
+** Sort 4 elements using the minimum extraction method.
+** First, rotate stack A until the element with pos=0 is at the top,
+** then push it to B. Next, sort the remaining 3 elements in A
+** using sort_three_by_pos(), and finally return the 0 from B to A,
+** leaving the stack completely sorted in ascending order.
+
+** Sort 5 elements by extracting the two smallest ones to B.
+** First, rotate A until you find pos=0, then push it to B.
+** Then rotate A until you find pos=1, and push it to B as well.
+** With the 3 remaining elements in A, sort them with sort_three_by_pos().
+** Finally, return the two elements from B in the correct order (1 then 0),
+** leaving stack A completely sorted in ascending order.
+*/
 
 void	sort_two_by_pos(t_stack **stack)
 {
